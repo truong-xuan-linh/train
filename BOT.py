19912ca9831fcd28ec55d50f0462753f9a16a67b
truong-xuan-linh <truonglinh1342001@gmail.com>
2021-03-21 22:29:40 +0700

    add BOT

---
+++ BOT.py
@@ -0,0 +1,18 @@
+n = int(input())
+a = list(map(int,input().split()))
+
+l = [a[0]]
+maxx = l[0]
+p = 1
+q = 1
+for i in range(1, n):
+    if a[i] > a[i] + l[i-1]:
+        l.append(a[i])
+        p = i +1
+    else:
+        l.append(a[i] + l[i-1])
+    if l[i] > maxx:
+        maxx = l[i]
+        q = i +1
+
+print(maxx,p,q)
